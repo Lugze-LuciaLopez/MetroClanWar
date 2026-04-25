079f13f5f623cb422a1059df7feb327364fb6976
Alejandro Casas <153321318+Kapustin1937@users.noreply.github.com>
2026-04-25 23:23:15 +0200

    implementat preguntes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /** @typedef {import('pear-interface')} */ /* global Pear */
 const { versions } = Pear
+console.log("your blutus divice is redy to pear")
 console.log('Pear terminal application running')
 console.log(await versions())
